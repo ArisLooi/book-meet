157f65ffcac183e08996edcc90898a6e592c997f
ArisLooi <arislooi@outlook.com>
2025-01-09 20:08:18 +0800

    Add RoomCard.jsx, import to page.jsx, add room images

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,13 @@
+import rooms from '@/data/rooms.json'
+import RoomCard from '@/components/RoomCard';
+
 export default function Home() {
   return (
     <>
-      <h1>Bookmeet App</h1>
+      {/* Check for room availability */}
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (<p>No rooms available at the moment</p>)}
     </>
   );
 }
