5584b99951407da822d6826bb7404d1f5a2e4994
ArisLooi <arislooi@outlook.com>
2025-01-09 22:05:01 +0800

    Update individual room page.jsx to be dynamic

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -35,107 +35,30 @@ const RoomPage = ({ params }) => { //params from the server side
 
                     <div className="mt-4 sm:mt-0 sm:flex-1">
                         <p className="text-gray-600 mb-4">
-                            Spacious conference hall perfect for large meetings and events.
-                            Equipped with modern amenities and comfortable seating.
+                            {room.description}
                         </p>
 
                         <ul className="space-y-2">
                             <li className="text-gray-800">
-                                <span className="font-semibold">Size:</span> 3000 sq
-                                ft
+                                <span className="font-semibold">Size:</span> {room.sqft}
                             </li>
                             <li className="text-gray-800">
                                 <span className="font-semibold">Availability:</span>
-                                9 AM - 5 PM
+                                {room.availability}
                             </li>
                             <li className="text-gray-800">
                                 <span className="font-semibold">Price:</span>
-                                $150/hour
+                                ${room.price_per_hour}hour
                             </li>
                             <li className="text-gray-800">
-                                <span className="font-semibold">Address:</span> 555
-                                California St, San Francisco, CA 94104
+                                <span className="font-semibold">Address:</span> {' '}
+                                {room.address}
                             </li>
                         </ul>
                     </div>
                 </div>
 
-                <div className="mt-6">
-                    <h2 className="text-xl font-bold text-gray-800">Book this Room</h2>
-                    <form className="mt-4">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                            <div>
-                                <label
-                                    htmlFor="check_in_date"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Check-In Date
-                                </label>
-                                <input
-                                    type="date"
-                                    id="check_in_date"
-                                    name="check_in_date"
-                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="check_in_time"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Check-In Time
-                                </label>
-                                <input
-                                    type="time"
-                                    id="check_in_time"
-                                    name="check_in_time"
-                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="check_out_date"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Check-Out Date
-                                </label>
-                                <input
-                                    type="date"
-                                    id="check_out_date"
-                                    name="check_out_date"
-                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor="check_out_time"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Check-Out Time
-                                </label>
-                                <input
-                                    type="time"
-                                    id="check_out_time"
-                                    name="check_out_time"
-                                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    required
-                                />
-                            </div>
-                        </div>
 
-                        <div className="mt-6">
-                            <button
-                                type="submit"
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800"
-                            >
-                                Book Room
-                            </button>
-                        </div>
-                    </form>
-                </div>
             </div>
         </>
     );
