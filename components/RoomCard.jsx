1d98b36f068f41eecf585e0149c30a552a2f9db0
ArisLooi <arislooi@outlook.com>
2025-01-09 20:50:10 +0800

    Amended rooms images file location and Footer.jsx className

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -6,7 +6,7 @@ const RoomCard = ({ room }) => {
             className="bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center"
         >
             <div className="flex flex-col sm:flex-row sm:space-x-4">
-                <Image
+                <img
                     src="images/rooms/room-1.jpg"
                     alt="Grand Conference Hall"
                     className="w-full sm:w-32 sm:h-32 mb-3 sm:mb-0 object-cover rounded-lg"
