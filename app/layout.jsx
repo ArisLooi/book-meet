2229930b171e124b5e6919ef9e2d63a587adf6fe
ArisLooi <arislooi@outlook.com>
2025-01-10 21:36:59 +0800

    Update Header.jsx, create and update AuthWrapper.jsx, import useAuth to page.jsx and layout.jsx, update middleware.js

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,10 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
+import AuthWrapper from '@/components/AuthWrapper';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,17 +16,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={inter.className}
-      >
-        <Header />
-        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          {children}
-        </main>
-        <Footer />
-        <ToastContainer />
-      </body>
-    </html>
+    <AuthWrapper>
+      <html lang="en">
+        <body
+          className={inter.className}
+        >
+          <Header />
+          <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            {children}
+          </main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthWrapper>
   );
 }
