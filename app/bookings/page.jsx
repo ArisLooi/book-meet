a0f7920583eb91714a7dd6a548b762170ebb66c2
ArisLooi <arislooi@outlook.com>
2025-01-13 22:54:42 +0800

    Initiate BookedRoomCard and getMyBookings, import BookedRoomCard to BookingsPage,

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,6 +1,21 @@
-const BookingsPage = () => {
+import Heading from "@/components/Heading";
+import getMyBookings from "../actions/getMyBookings";
+import BookedRoomCard from "@/components/BookedRoomCard";
+
+const BookingsPage = async () => {
+    const bookings = await getMyBookings();
     return (
-        <>Bookings</>
+        <>
+            {
+                bookings.length === 0 ? (
+                    <p className='text-gray-600 mt-4'>You have no bookings</p>
+                ) : (
+                    bookings.map((booking) => <BookedRoomCard key={booking.$id}
+                        booking={booking} />
+                    )
+                )
+            }
+        </>
     );
 }
-export default BookingsPage
+export default BookingsPage;
