78154e47f63fc5044c5b250b06e341b5d6f7f9a7
ArisLooi <arislooi@outlook.com>
2025-01-13 15:44:18 +0800

    Update RoomCard with images storage bucket, update createRoom with image extraction from database, import createRoom to AddRoomPage, configure next.js for images

--- app/rooms/add/page.jsx
+++ app/rooms/add/page.jsx
@@ -1,61 +1,76 @@
+'use client';
+import { useEffect, useActionState } from 'react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 import Heading from '@/components/Heading'
+import createRoom from '@/app/actions/createRoom';
 
 const AddRoomPage = () => {
+    const [state, submitAction] = useActionState(createRoom, {});
+    const router = useRouter();
+
+    useEffect(() => {
+        if (state.error) toast.error(state.error);
+        if (state.success) {
+            toast.success('Room create successfully!');
+            router.push('/');
+        }
+    })
     return (
         <>
             <Heading title='Add a Room' />
             <div className="bg-white shadow-lg rounded-lg p-6 w-full">
-                <form>
+                <form action={submitAction}>
                     <div className="mb-4">
-                        <label for="name" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="name" className="block text-gray-700 font-bold mb-2"
                         >Room Name</label
                         >
                         <input
                             type="text"
                             id="name"
                             name="name"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Enter a name (Large Conference Room)"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="description" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="description" className="block text-gray-700 font-bold mb-2"
                         >Description</label
                         >
                         <textarea
                             id="description"
                             name="description"
-                            className="border rounded w-full h-24 py-2 px-3"
+                            className="border rounded w-full h-24 py-2 px-3 text-gray-700"
                             placeholder="Enter a description for the room"
                             required
                         ></textarea>
                     </div>
 
                     <div className="mb-4">
-                        <label for="sqft" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="sqft" className="block text-gray-700 font-bold mb-2"
                         >Square Feet</label
                         >
                         <input
                             type="number"
                             id="sqft"
                             name="sqft"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Enter room size in ft"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="capacity" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="capacity" className="block text-gray-700 font-bold mb-2"
                         >Capacity</label
                         >
                         <input
                             type="number"
                             id="capacity"
                             name="capacity"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Number of people the room can hold"
                             required
                         />
@@ -72,63 +87,63 @@ const AddRoomPage = () => {
                             type="number"
                             id="price_per_hour"
                             name="price_per_hour"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Enter price per hour"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="address" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="address" className="block text-gray-700 font-bold mb-2"
                         >Address</label
                         >
                         <input
                             type="text"
                             id="address"
                             name="address"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Enter full address"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="location" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="location" className="block text-gray-700 font-bold mb-2"
                         >Location</label
                         >
                         <input
                             type="text"
                             id="location"
                             name="location"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Location (Building, Floor, Room)"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="availability" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="availability" className="block text-gray-700 font-bold mb-2"
                         >Availability</label
                         >
                         <input
                             type="text"
                             id="availability"
                             name="availability"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Availability (Monday - Friday, 9am - 5pm)"
                             required
                         />
                     </div>
 
                     <div className="mb-4">
-                        <label for="amenities" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="amenities" className="block text-gray-700 font-bold mb-2"
                         >Amenities</label
                         >
                         <input
                             type="text"
                             id="amenities"
                             name="amenities"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                             placeholder="Amenities CSV (projector, whiteboard, etc.)"
                             required
                         />
@@ -136,7 +151,7 @@ const AddRoomPage = () => {
 
                     {/* <!-- Image Upload --> */}
                     <div className="mb-8">
-                        <label for="image" className="block text-gray-700 font-bold mb-2"
+                        <label htmlFor="image" className="block text-gray-700 font-bold mb-2"
                         >Image</label
                         >
 
@@ -144,7 +159,7 @@ const AddRoomPage = () => {
                             type="file"
                             id="image"
                             name="image"
-                            className="border rounded w-full py-2 px-3"
+                            className="border rounded w-full py-2 px-3 text-gray-700"
                         />
                     </div>
 
@@ -152,8 +167,9 @@ const AddRoomPage = () => {
                         <button
                             type="submit"
                             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-                        />
-                        Save
+                        >
+                            Save
+                        </button>
                     </div>
                 </form>
             </div>
